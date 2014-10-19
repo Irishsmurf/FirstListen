a3b9c9cb8ea3db55cc4d87bedc359e27fde20351
Paddez <dave@paddez.com>
2014-10-19 14:13:18 +0000

    Original HTML

---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+
+    <title> First time listened - Last.fm </title>
+    <link rel="stylesheet" href="https://www.paddez.com/style.css" type="text/css" media="screen" />
+
+</head>
+
+<body>
+    <div id="content">
+    <div id="main-content">
+    <section id=intro">
+    <header>
+        <h2>Last.fm - First time listened to a band </h2>
+    </header>
+        <p> Lorum </p>
+    </section>
+    </div>
+    </div>
+   </body>
+
+</html>
